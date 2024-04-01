0874da0f1581a68834efd8aef96662ef65b80ede
Malte <malte.menger@n-ergie.de>
2024-04-01 18:13:28 +0200

    fixed method signature

--- src/services/AuthenticationDataService.ts
+++ src/services/AuthenticationDataService.ts
@@ -2,22 +2,23 @@ import { CustomerAuthenticationData } from "../../@cds-models/voicebot/typescrip
 import * as cds from '@sap/cds'
 
 export interface IAuthenticationDataService {
-    async get_existing_auth_informations_for_businesspartner(kundennummer: string): Promise<CustomerAuthenticationData>
-    async get_authentication_data_for_businesspartner(kundennummer: string): Promise<CustomerAuthenticationData>
+    get_existing_auth_informations_for_businesspartner(kundennummer: string): Promise<CustomerAuthenticationData>
+    get_authentication_data_for_businesspartner(kundennummer: string): Promise<CustomerAuthenticationData>
 }
 
 
 export class AuthenticationDataService implements IAuthenticationDataService {
     
     async get_existing_auth_informations_for_businesspartner(kundennummer: string): Promise<CustomerAuthenticationData> {
-
         const srv = await cds.connect.to ('AuthProvider');
         const { AuthProvider } = srv.entities;
         return await srv.run (SELECT.from(AuthProvider));
     }
 
     async get_authentication_data_for_businesspartner(kundennummer: string): Promise<CustomerAuthenticationData> {
-        throw new Error("Method not implemented.");
+        const srv = await cds.connect.to ('AuthProvider');
+        const { AuthProvider } = srv.entities;
+        return await srv.run (SELECT.from(AuthProvider));
     }
 
 }
